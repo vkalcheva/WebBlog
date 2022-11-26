5fb618b2f898f22888000cd032ab2e1084a3a1e1
Violeta Kalcheva <violetakalcheva@abv.bg>
2022-11-26 17:40:22 +0200

    CRUD-update and delete project functionalty

--- projects/views.py
+++ projects/views.py
@@ -35,3 +35,30 @@ def create_project(request):
         'form': form
     }
     return render(request, "projects/project_form.html", context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form =ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        'form': form
+    }
+    return render(request, "projects/project_form.html", context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {
+        'object': project
+    }
+    return render(request, "projects/delete_template.html", context)
